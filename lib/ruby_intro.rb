e80431fd4b60216031dc456432f24a611dbcacfa
EC2 Default User <ec2-user@ip-172-31-22-191.us-east-2.compute.internal>
2020-01-31 22:42:18 +0000

    Finished ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,89 @@
-# When done, submit this entire file to the autograder.
+#----- SOURCES USED -----#
 
-# Part 1
+# https://www.geeksforgeeks.org/ruby-getters-and-setters-method/
+# https://rubular.com/
+# https://www.ruby-lang.org/en/documentation/
+# https://stackoverflow.com/questions/2174767/extracting-the-last-n-characters-from-a-ruby-string
 
-def sum arr
-  # YOUR CODE HERE
+#----- PART 1: Arrays, Hashes, and Enumerables -----#
+
+def sum(array)
+    sum = 0;
+    array.each do |n|
+       sum = sum + n 
+    end
+    return sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(array)
+    sum = 0;
+    max_array = array.max(2)
+    max_array.each do |n|
+        sum = sum + n
+    end
+    return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(array, n)
+    count = 1
+    array.each do |x|
+        smol_array = array.drop(count)
+        count = count + 1
+        smol_array.each do |y|
+            if (x + y == n) then
+                return true
+            end
+        end
+    end
+    return false
 end
 
-# Part 2
+#----- PART 2: Strings and Regular Expressions -----#
 
 def hello(name)
-  # YOUR CODE HERE
+    return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+    if (!s.match(/\A[A-z]/)) then                            #Ensure s starts with a letter
+        return false
+    end
+    if (s.match(/\A[AaEeIiOoUu]/))                           #If s starts with vowel, return fals
+        return false
+    end
+    return true
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+    if (s.match(/\D[^2-9]/) || s.match(/[2-9]/)) then        #If s has anything thats not a number
+        return false                                         #or numbers 2-9, return false
+    end
+    
+    if s.split('').last(2).join() == '00' || s == '0' then   #Check if last two digits are 0 (multiple
+        return true                                          #or 4), or if s = 0, then return true
+    end
+   
+    return false
 end
 
-# Part 3
+#----- PART 3: Object Oriented Basics -----#
 
 class BookInStock
-# YOUR CODE HERE
+   def initialize(isbn, price)
+       if (isbn.empty? || price < 0 || price == 0) then
+          raise ArgumentError 
+       end
+       @isbn = isbn
+       @price = price
+   end
+
+   #getters and setters
+   attr_reader :isbn
+   attr_writer :isbn
+   attr_reader :price
+   attr_writer :price
+   
+   def price_as_string()
+      return "$%0.2f" % [@price] 
+   end
 end
